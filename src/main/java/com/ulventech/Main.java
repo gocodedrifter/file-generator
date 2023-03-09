6314c6766af2a3943fec24fe28befcaf0fc5121b
arip <=>
2023-03-09 11:02:56 +0700

    implement the command

--- src/main/java/com/ulventech/Main.java
+++ src/main/java/com/ulventech/Main.java
@@ -1,5 +1,23 @@
 package com.ulventech;
 
-public class Main {
+import com.ulventech.command.GenerateFileCommand;
+import com.ulventech.command.Invoker;
 
+public class Main {
+	public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Please provide a number as an argument");
+        }
+
+        int n = Integer.parseInt(args[0]);
+
+        if (n <= 0 || n >= Math.pow(2, 30)) {
+            throw new IllegalArgumentException("Invalid input parameters");
+        }
+
+        GenerateFileCommand generateFileCommand = new GenerateFileCommand();
+        Invoker invoker = new Invoker();
+        invoker.setCommand(generateFileCommand);
+        invoker.executeCommand(n);
+    }
 }
